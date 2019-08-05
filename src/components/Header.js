0d2184f36613b21284231359a7786c68712f2d0d
meganrm <meganrm@gmail.com>
2019-08-05 15:51:27 -0700

    fix apostrophy

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
     <h1>Stand Up for Safer Gun Laws</h1>
     <p>Nine people are dead and 26 people were injured after a mass shooting in Dayton, Ohio.
         The incident took place within 13 hours of the shooting in El Paso, Texas where at least 20 people were killed and 26 more wounded.
-        On top of that Chicago&#39s Mount Sinai Hospital temporarily stopped accepting patients Sunday morning because they were at capacity following a series of shootings.
+        On top of that Chicago&#39;s Mount Sinai Hospital temporarily stopped accepting patients Sunday morning because they were at capacity following a series of shootings.
       <strong> This is not normal. </strong>
         The Gun Violence Prevention community is calling on Mitch McConnell and Donald Trump to cancel August recess and finally take action to address this horrific national public safety threat.
       <br />#BringThemBack
