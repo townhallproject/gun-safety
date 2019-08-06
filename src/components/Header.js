0a7981bb5ecd6a3a98bb44d4566c4d0da48d0f93
meganrm <meganrm@gmail.com>
2019-08-06 10:11:43 -0700

    removed additional line

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
     <h1>Join the Movement for Gun Safety</h1>
     <p>46 people were shot in an El Paso Walmart. 36 were shot in Dayton, Ohio. So many people were shot in Chicago that Mt. Sinai Hospital couldn’t accept any more patients. After a tragic and violent 48 hours from August 3rd to August 4th, the country is asking what they can do to end our gun violence epidemic.
     </p>
-    <p>The Gun Violence Prevention community is calling for action: <strong>NOW is the time to demand that Mitch McConnell suspend the Senate recess and bring forward legislation to meaningfully address American gun violence. NOW is the time to demand that Mitch McConnell bring forward legislation to meaningfully address American gun violence. NOW is the time to flood your local town halls and district offices and meet your lawmakers face-to-face. NOW is the time to hold our elected officials accountable and remind them of the power of your vote come November.</strong>
+    <p>The Gun Violence Prevention community is calling for action: <strong>NOW is the time to demand that Mitch McConnell suspend the Senate recess and bring forward legislation to meaningfully address American gun violence. NOW is the time to flood your local town halls and district offices and meet your lawmakers face-to-face. NOW is the time to hold our elected officials accountable and remind them of the power of your vote come November.</strong>
     </p>
     <a
       target="_blank"
