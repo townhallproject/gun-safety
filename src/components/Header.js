4ca46a3f8470915dbd1618cb5e7f38a177dd4087
meganrm <meganrm@gmail.com>
2019-08-06 00:00:46 -0700

    new copy

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import React from 'react';
 const Header = () => (
   <div className="header">
     <h1>Stand Up for Safer Gun Laws</h1>
-    <p>Nine people are dead and 26 people were injured after a mass shooting in Dayton, Ohio.
-        The incident took place within 13 hours of the shooting in El Paso, Texas where at least 20 people were killed and 26 more wounded.
-        On top of that Chicago&#39;s Mount Sinai Hospital temporarily stopped accepting patients Sunday morning because they were at capacity following a series of shootings.
-      <strong> This is not normal. </strong>
-        The Gun Violence Prevention community is calling on Mitch McConnell and Donald Trump to cancel August recess and finally take action to address this horrific national public safety threat.
-      <br />#BringThemBack
+    <p>46 people were shot in an El Paso Walmart. 36 were shot in Dayton, Ohio. So many people were shot in Chicago that Mt. Sinai Hospital couldn’t accept any more patients. After a tragic and violent 48 hours from August 3rd to August 4th, the country is asking what they can do to end our gun violence epidemic.
     </p>
+    <p>
+      The Gun Violence Prevention community is calling for action: <strong>NOW is the time to demand that Mitch McConnell suspend the Senate recess and bring forward legislation to meaningfully address American gun violence. NOW is the time to demand that Mitch McConnell bring forward legislation to meaningfully address American gun violence. NOW is the time to flood your local town halls and district offices and meet your lawmakers face-to-face. NOW is the time to hold our elected officials accountable and remind them of the power of your vote come November.</strong>
+    </p>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://docs.google.com/document/d/1Go1GsYldM9iRLzpvQP1GZVjRXBjJ7Qcm8Xk_ku86lEQ/edit#">Click here to view our toolkit for details on how you can take action.  </a>
   </div>
 );
 export default Header;
