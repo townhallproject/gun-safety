a69ec84a5471b8a6c78bfa861ed03a79ce40372b
meganrm <meganrm@gmail.com>
2019-08-06 09:49:07 -0700

    changed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 const Header = () => (
   <div className="header">
-    <h1>Stand Up for Safer Gun Laws</h1>
+    <h1>Join the Movement for Gun Safety</h1>
     <p>46 people were shot in an El Paso Walmart. 36 were shot in Dayton, Ohio. So many people were shot in Chicago that Mt. Sinai Hospital couldn’t accept any more patients. After a tragic and violent 48 hours from August 3rd to August 4th, the country is asking what they can do to end our gun violence epidemic.
     </p>
-    <p>
-      The Gun Violence Prevention community is calling for action: <strong>NOW is the time to demand that Mitch McConnell suspend the Senate recess and bring forward legislation to meaningfully address American gun violence. NOW is the time to demand that Mitch McConnell bring forward legislation to meaningfully address American gun violence. NOW is the time to flood your local town halls and district offices and meet your lawmakers face-to-face. NOW is the time to hold our elected officials accountable and remind them of the power of your vote come November.</strong>
+    <p>The Gun Violence Prevention community is calling for action: <strong>NOW is the time to demand that Mitch McConnell suspend the Senate recess and bring forward legislation to meaningfully address American gun violence. NOW is the time to demand that Mitch McConnell bring forward legislation to meaningfully address American gun violence. NOW is the time to flood your local town halls and district offices and meet your lawmakers face-to-face. NOW is the time to hold our elected officials accountable and remind them of the power of your vote come November.</strong>
     </p>
     <a
       target="_blank"
       rel="noopener noreferrer"
-      href="https://docs.google.com/document/d/1Go1GsYldM9iRLzpvQP1GZVjRXBjJ7Qcm8Xk_ku86lEQ/edit#">Click here to view our toolkit for details on how you can take action.  </a>
+      href="https://docs.google.com/document/d/1Go1GsYldM9iRLzpvQP1GZVjRXBjJ7Qcm8Xk_ku86lEQ/edit#"
+    >Click here to view our toolkit for details on how you can take action.
+    </a>
   </div>
 );
 export default Header;
